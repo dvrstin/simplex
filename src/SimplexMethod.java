47edb5adbb898f5bcce23277b4a81a56fe476f0b
MaxNks02 <maman.nks02@gmail.com>
2024-10-11 19:50:38 +0300

    first commit

---
+++ src/SimplexMethod.java
@@ -0,0 +1,317 @@
+import java.util.Scanner;
+
+public class SimplexMethod {
+    private static final double EPS = 1e-9;
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Test Case 0:");
+        double[] c0 = {4, 5};
+        double[][] a0 = {
+                {2, 4},
+                {1, 1},
+                {2, 1}
+        };
+        double[] b0 = {560, 170, 300};
+        exeSimp(c0, a0, b0, "max");
+
+        System.out.println("Test Case 1:");
+        double[] c1 = {40, 30};
+        double[][] a1 = {
+                {1, 1},
+                {2, 1},
+                {-1, 0},
+                {0, -1}
+        };
+        double[] b1 = {12, 16, 0, 0};
+        exeSimp(c1, a1, b1, "max");
+
+
+        System.out.println("Test Case 2:");
+        double[] c2 = {3, 5, 4};
+        double[][] a2 = {
+                {2, 3, 0},
+                {0, 2, 5},
+                {3, 2, 4},
+                {-1, 0, 0},
+                {0, -1, 0},
+                {0, 0, -1},
+        };
+        double[] b2 = {8, 10, 15, 0, 0, 0};
+        exeSimp(c2, a2, b2, "max");
+
+
+        System.out.println("Test Case 3:");
+        double[] c3 = {3, 2};
+        double[][] a3 = {
+                {2, 1},
+                {2, 3},
+                {3, 1},
+                {-1, 0},
+                {0, -1},
+        };
+        double[] b3 = {18, 42, 24, 0, 0};
+        exeSimp(c3, a3, b3, "max");
+
+
+        System.out.println("Test Case 4:");
+        double[] c4 = {4, 1};
+        double[][] a4 = {
+                {1, 1},
+                {3, 1},
+                {-1, 0},
+                {0, -1}
+        };
+        double[] b4 = {50, 90, 0, 0};
+        exeSimp(c4, a4, b4, "max");
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of variables of objective function:");
+        int nVars = sc.nextInt();
+        System.out.println("Enter the number of constraints:");
+        int nConstrs = sc.nextInt();
+
+        double[] c = new double[nVars];
+        System.out.println("Enter the coefficients of the objective function (a,b, ...): Z= ax1 + bx2+...");
+        for (int i = 0; i < nVars; i++) {
+            c[i] = sc.nextDouble();
+        }
+
+        double[][] a = new double[nConstrs][nVars];
+        double[] b = new double[nConstrs];
+        System.out.println("Enter the coefficients of the constraints (a, b, ... and vector B): ax1 + bx2 + ... <= vecB");
+        for (int i = 0; i < nConstrs; i++) {
+            for (int j = 0; j < nVars; j++) {
+                a[i][j] = sc.nextDouble();
+            }
+            b[i] = sc.nextDouble();
+        }
+
+
+        printProblem(c, a, b, "max");
+
+        double[] res = simplex(c, a, b);
+
+        if (res == null) {
+            System.out.println("No solution exists or the problem is unbounded!");
+        } else {
+            System.out.println("Solution found:");
+            for (int i = 0; i < res.length - 1; i++) {
+                System.out.printf("x%d = %.5f%n", i + 1, res[i]);
+            }
+            System.out.printf("Objective function value: %.5f%n", res[res.length - 1]);
+        }
+
+    }
+
+    private static void exeSimp(double[] c, double[][] a, double[] b, String optimizationType) {
+
+        printProblem(c, a, b, optimizationType);
+
+        double[] res = simplex(c, a, b);
+
+        if (res == null) {
+            System.out.println("No solution exists or the problem is unbounded!");
+        } else {
+            System.out.println("Solution found:");
+            for (int i = 0; i < res.length - 1; i++) {
+                System.out.printf("x%d = %.5f%n", i + 1, res[i]);
+            }
+            System.out.printf("Objective function value: %.5f%n", res[res.length - 1]);
+        }
+        System.out.println();
+    }
+
+    private static void printProblem(double[] c, double[][] a, double[] b, String optimizationType) {
+        System.out.println("Optimization problem:");
+        System.out.printf("   - %s z = ", optimizationType.equalsIgnoreCase("max") ? "max" : "min");
+        for (int i = 0; i < c.length; i++) {
+            if (i > 0) System.out.print(" + ");
+            System.out.printf("%.2f*x%d", c[i], i + 1);
+        }
+        System.out.println();
+        System.out.println("   - subject to the constraints:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("     - ");
+            for (int j = 0; j < a[i].length; j++) {
+                if (j > 0) System.out.print(" + ");
+                System.out.printf("%.2f*x%d", a[i][j], j + 1);
+            }
+            System.out.printf(" <= %.2f%n", b[i]);
+        }
+        System.out.println();
+    }
+
+    private static double[] simplex(double[] c, double[][] a, double[] b) {
+        int nVars = c.length;
+        int nConstrs = a.length;
+
+        double[][] tbl = new double[nConstrs + 1][nVars + nConstrs + 1];
+
+
+        for (int i = 0; i < nConstrs; i++) {
+            for (int j = 0; j < nVars; j++) {
+                tbl[i][j] = a[i][j];
+            }
+            tbl[i][nVars + i] = 1.0;
+            tbl[i][nVars + nConstrs] = b[i];
+        }
+
+        for (int j = 0; j < nVars; j++) {
+            tbl[nConstrs][j] = -c[j];
+        }
+
+        int iter = 0;
+
+        while (true) {
+            iter++;
+            System.out.printf("Iteration %d:%n", iter);
+            printConstr(tbl, nVars, nConstrs);
+
+
+            int pCol = -1;
+            double mostNeg = 0;
+            for (int j = 0; j < nVars + nConstrs; j++) {
+                if (tbl[nConstrs][j] < mostNeg - EPS) {
+                    mostNeg = tbl[nConstrs][j];
+                    pCol = j;
+                }
+            }
+
+            if (pCol == -1) {
+
+                double[] res = new double[nVars + 1];
+                for (int i = 0; i < nConstrs; i++) {
+                    for (int j = 0; j < nVars; j++) {
+                        if (Math.abs(tbl[i][j] - 1.0) < EPS) {
+                            res[j] = tbl[i][nVars + nConstrs];
+                        }
+                    }
+                }
+                res[nVars] = tbl[nConstrs][nVars + nConstrs];
+                return res;
+            }
+
+
+            int pRow = -1;
+            double minRatio = Double.MAX_VALUE;
+            boolean hasValidPivot = false;
+
+            for (int i = 0; i < nConstrs; i++) {
+
+                if (tbl[i][pCol] > EPS) {
+                    double ratio = tbl[i][nVars + nConstrs] / tbl[i][pCol];
+                    hasValidPivot = true;
+                    if (ratio < minRatio) {
+                        minRatio = ratio;
+                        pRow = i;
+                    }
+                }
+            }
+
+
+            if (!hasValidPivot) {
+                System.out.println("The problem is unbounded!");
+                return null;
+            }
+
+            System.out.printf("Selected pivot element in row %d and column %d:%n", pRow + 1, pCol + 1);
+            printConstrP(tbl, nVars, nConstrs, pRow, pCol);
+            piv(tbl, pRow, pCol, nVars, nConstrs);
+        }
+    }
+
+    private static void piv(double[][] tbl, int pRow, int pCol, int nVars, int nConstrs) {
+        int nRows = tbl.length;
+        int nCols = tbl[0].length;
+
+        double pVal = tbl[pRow][pCol];
+
+        for (int j = 0; j < nCols; j++) {
+            tbl[pRow][j] /= pVal;
+        }
+
+        for (int i = 0; i < nRows; i++) {
+            if (i != pRow) {
+                double factor = tbl[i][pCol];
+                for (int j = 0; j < nCols; j++) {
+                    tbl[i][j] -= factor * tbl[pRow][j];
+                }
+            }
+        }
+    }
+
+    private static void printConstr(double[][] tbl, int nVars, int nConstrs) {
+        for (int i = 0; i < nConstrs; i++) {
+            System.out.print("  ");
+            for (int j = 0; j < nVars + nConstrs; j++) {
+                if (j < nVars) {
+                    System.out.printf("%.2f*x%d ", tbl[i][j], j + 1);
+                } else {
+                    System.out.printf("%.2f*s%d ", tbl[i][j], j - nVars + 1);
+                }
+                if (j < nVars + nConstrs - 1) {
+                    System.out.print("+ ");
+                }
+            }
+            System.out.printf("= %.2f%n", tbl[i][nVars + nConstrs]);
+        }
+
+        System.out.print("  ");
+        for (int j = 0; j < nVars + nConstrs; j++) {
+            if (j < nVars) {
+                System.out.printf("%.2f*x%d ", tbl[nConstrs][j], j + 1);
+            } else {
+                System.out.printf("%.2f*s%d ", tbl[nConstrs][j], j - nVars + 1);
+            }
+            if (j < nVars + nConstrs - 1) {
+                System.out.print("+ ");
+            }
+        }
+        System.out.printf("= %.2f%n", tbl[nConstrs][nVars + nConstrs]);
+    }
+
+    private static void printConstrP(double[][] tbl, int nVars, int nConstrs, int pRow, int pCol) {
+        System.out.println("Current simplex tableau with the selected pivot element:");
+
+        for (int i = 0; i < nConstrs; i++) {
+            System.out.print("  ");
+            for (int j = 0; j < nVars + nConstrs; j++) {
+                String format = "%.2f";
+                if (i == pRow && j == pCol) {
+                    format = ANSI_RED + "%.2f" + ANSI_RESET;
+                }
+                if (j < nVars) {
+                    System.out.printf(format + "*x%d ", tbl[i][j], j + 1);
+                } else {
+                    System.out.printf(format + "*s%d ", tbl[i][j], j - nVars + 1);
+                }
+                if (j < nVars + nConstrs - 1) {
+                    System.out.print("+ ");
+                }
+            }
+            System.out.printf("= %.2f%n", tbl[i][nVars + nConstrs]);
+        }
+
+        System.out.print("  ");
+        for (int j = 0; j < nVars + nConstrs; j++) {
+            String format = "%.2f";
+            if (nConstrs == pRow && j == pCol) {
+                format = ANSI_RED + "%.2f" + ANSI_RESET;
+            }
+            if (j < nVars) {
+                System.out.printf(format + "*x%d ", tbl[nConstrs][j], j + 1);
+            } else {
+                System.out.printf(format + "*s%d ", tbl[nConstrs][j], j - nVars + 1);
+            }
+            if (j < nVars + nConstrs - 1) {
+                System.out.print("+ ");
+            }
+        }
+        System.out.printf("= %.2f%n%n", tbl[nConstrs][nVars + nConstrs]);
+    }
+}
